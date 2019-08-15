339eb818d2467c127260492e1f114f07f6921b20
Ruth Poliakon <r.poliakon@gmail.com>
2019-08-15 08:44:33 -0600

    data passed to component function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,33 +1,33 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+  (replacing the palceholder with your Github name):
+  https://api.github.com/users/<your name>
 */
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+  github info! You will need to understand the structure of this 
+  data in order to use it to build your component function 
 
-   Skip to Step 3.
+  Skip to Step 3.
 */
 
 /* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
+  create a new component and add it to the DOM as a child of .cards
 */
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
+  follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+  , manually find some other users' github handles, or use the list found 
+  at the bottom of the page. Get at least 5 different Github usernames and add them as
+  Individual strings to the friendsArray below.
+  
+  Using that array, iterate over it, requesting data for each user, creating a new card for each
+  user, and adding that card to the DOM.
 */
 
 const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+  Using DOM methods and properties, create a component that will return the following DOM element:
 
 <div class="card">
   <img src={image url of user} />
@@ -105,19 +105,16 @@ function gitCard(
   return card;
 }
 
-const ruth = gitCard(
-  "https://avatars0.githubusercontent.com/u/47793431?v=4",
-  "name",
-  "login",
-  "location",
-  "html_url",
-  "followers",
-  "following",
-  "bio"
-);
-
-const container = document.querySelector(".cards");
-container.appendChild(ruth);
+// const ruth = gitCard(
+//   "https://avatars0.githubusercontent.com/u/47793431?v=4",
+//   "name",
+//   "login",
+//   "location",
+//   "html_url",
+//   "followers",
+//   "following",
+//   "bio"
+// );
 
 /* List of LS Instructors Github username's: 
   tetondan
@@ -127,6 +124,25 @@ container.appendChild(ruth);
   bigknell
 */
 
-axios.get("https://api.github.com/users/rupol").then(response => {
-  console.log(response);
-});
+axios
+  .get("https://api.github.com/users/rupol")
+  .then(response => {
+    console.log(response.data);
+    // network request finished
+    const card = gitCard(
+      response.data.avatar_url,
+      response.data.name,
+      response.data.login,
+      response.data.location,
+      response.data.html_url,
+      response.data.followers,
+      response.data.following,
+      response.data.bio
+    );
+    const container = document.querySelector(".cards");
+    container.appendChild(card);
+  })
+  .catch(error => {
+    console.log("Network request was unsuccessful");
+    console.log(error);
+  });
